89cae50e6d8d766889c161c2ed118ea33c2b8712
RedamancyAY <1594205849@qq.com>
2025-08-23 20:16:34 +0800

    update main

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -14,6 +14,7 @@ def get_full_gs_data():
 
     print("准备获取 author id...", flush=True)
     try:
+        print("使用 GOOGLE_SCHOLAR_ID 环境变量中的 ID 进行查询", os.environ["GOOGLE_SCHOLAR_ID"], flush=True)
         author = scholarly.search_author_id(os.environ["GOOGLE_SCHOLAR_ID"])
         print(f"已获取 author id: {author.get('scholar_id', '未知')}", flush=True)
     except Exception as e:
